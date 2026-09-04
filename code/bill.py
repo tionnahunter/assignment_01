5a17d1500c1d6cc764ea6338a28a566d9451a8d4
Ti'onna Hunter <tionnadhunter@gmail.com>
2026-09-04 21:20:20 +0000

    Implement bill.py functions

--- code/bill.py
+++ code/bill.py
@@ -18,8 +18,7 @@ def tip_amount(subtotal, pct):
     >>> tip_amount(50, 20)
     10.0
     """
-    # TODO: your code here
-    pass
+    return round(subtotal * pct / 100, 2)
 
 
 def grand_total(subtotal, pct):
@@ -30,8 +29,8 @@ def grand_total(subtotal, pct):
     >>> grand_total(50, 20)
     60.0
     """
-    # TODO: your code here
-    pass
+    #return tip_amount(subtotal,pct)
+    return round(subtotal + tip_amount(subtotal, pct),2)
 
 
 def split_evenly(total, people):
@@ -42,8 +41,9 @@ def split_evenly(total, people):
     >>> split_evenly(60, 4)
     15.0
     """
-    # TODO: your code here
-    pass
+    if people <= 0:
+        raise ValueError("people must be greater than 0")
+    return round(total/people,2)
 
 
 def is_generous(pct):
@@ -52,5 +52,4 @@ def is_generous(pct):
     >>> is_generous(20)
     True
     """
-    # TODO: your code here
-    pass
+    return pct >= 20
